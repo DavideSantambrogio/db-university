e218cab762d89145516170aa4a2e71b414a5a48f
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-12-20 20:17:52 +0100

    mi ero dimenticato di puschare

--- query.sql
+++ query.sql
@@ -59,3 +59,21 @@ SELECT
     MAX(id)
 FROM
     departments;
+
+-- oppure
+
+SELECT
+    COUNT(*)
+FROM
+    departments;
+
+-- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+
+SELECT
+    COUNT(*)
+FROM
+    teachers
+WHERE
+    phone IS NULL;
+
+
