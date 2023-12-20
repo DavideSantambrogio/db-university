7cb5a3c73350cc014bc90f5ed0b6453dceb9205f
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-12-20 17:02:27 +0100

    add: 20/06/2020 exam with hour > 14 quary

--- query.sql
+++ query.sql
@@ -32,3 +32,14 @@ FROM
     courses
 WHERE
     period = "I semestre" AND year = 1;
+
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+
+SELECT
+    *
+FROM
+    exams
+WHERE
+    DATE = '2020-06-20' AND EXTRACT(HOUR
+FROM
+    HOUR) >= 14;
