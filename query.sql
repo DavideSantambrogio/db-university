55311c3ed174f4e4db544514931842781d37e1db
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-12-20 16:25:58 +0100

    add cfu >10 query

--- query.sql
+++ query.sql
@@ -1,5 +1,18 @@
 -- 1. Selezionare tutti gli studenti nati nel 1990
-SELECT `name`, `surname`, `date_of_birth`
-FROM `students`
-WHERE YEAR(`date_of_birth`) = 1990;
+SELECT
+    `name`,
+    `surname`,
+    `date_of_birth`
+FROM
+    `students`
+WHERE
+    YEAR(`date_of_birth`) = 1990;
 
+-- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+
+SELECT
+    *
+FROM
+    courses
+WHERE
+    cfu > 10;
