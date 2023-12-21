7e61db669f1139185f67b3a31e664b224363bba1
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-12-21 16:35:58 +0100

    add courses in department query

--- EX_GROUP_BY.sql
+++ EX_GROUP_BY.sql
@@ -15,3 +15,9 @@ GROUP BY `office_address`;
 SELECT `exam_id`, AVG(`exam_student`.`vote`) AS `avg_vote`
 FROM `exam_student`
 GROUP BY `exam_id`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT `department_id`, COUNT(*) AS `num_degrees`
+FROM `degrees`
+GROUP BY `department_id`;
