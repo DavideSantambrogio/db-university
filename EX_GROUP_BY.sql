90054af3b17ef71d1066a2612adcc79fe26e815f
DavideSantambrogio <santambrogio.davide.97@gmail.com>
2023-12-21 16:17:56 +0100

    office_adress query

--- EX_GROUP_BY.sql
+++ EX_GROUP_BY.sql
@@ -3,3 +3,9 @@
 SELECT YEAR(`enrolment_date`), COUNT(*) AS `num_students`
 FROM `students`
 GROUP BY YEAR(`enrolment_date`);
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT  `office_address` ,COUNT(*) AS `total_teachers`
+FROM `teachers`
+GROUP BY `office_address`;
